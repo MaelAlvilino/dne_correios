1923abedbe98010e0512b8c1db6c5c21ebce2af4
MaelAlvilino <emmanuel.alvi@hotmail.com>
2024-01-10 21:19:52 -0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
-import { type MiddlewareConsumer, Module } from '@nestjs/common';
+import {
+   type MiddlewareConsumer,
+   Module } from '@nestjs/common';
 import { ParseDneModule } from './modules/readfiles/parsedne.module';
 import {  findCepModule } from './modules/endereco/findcep.module';
 import { JWTAuthCorreios } from './security/jwt.middleware';
 
 @Module({
-  imports: [ParseDneModule,  findCepModule],
+  imports: [ParseDneModule,  findCepModule], 
 
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer): void {
+  configure(
+    consumer: MiddlewareConsumer
+    ): void {
     consumer.apply(JWTAuthCorreios).forRoutes('*');
   }
 }
